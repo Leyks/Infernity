8b4f9735c38a5560984937cf3eb48d3d694bbb06
Leyks <49054110+Leyks@users.noreply.github.com>
2019-03-29 19:31:07 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -7,16 +7,19 @@ bot.on('ready', function () {
 
 	)
 
-/*client.on('guildMemberAdd', member => {
-  const channel = member.guild.channels.find(ch => ch.name === 'member-log')
-  if (!channel) return
-  channel.send(`Bienvenue sur le serveur, ${member} ! N'ésite pas aller voir les #infos_du_clan et les #règles ainsi que de rejoindre le clan si ce n'est pas déjà fait !`)
-}) */
+bot.on("guildMemberAdd", member => {
+	member.guild.channels.find("name", "general").send('Bienvenue ${member}')
+})
+
+bot.on("guildMemberRemove", member => {
+	member.guild.channels.find("name", "general").send('${member} vient de quitter le serveur')
+})
+
 
 bot.on('guildMemberAdd', function (member) {
 	member.createDM().then(function(channel) {
 		return channel.send('Bienvenue sur le serveur ' + member.displayName)
-		//général.sendMessage ('Bienvenue sur le serveur ' + member.displayName + " ! N'ésite pas aller voir les #infos_du_clan et les #règles ainsi que de rejoindre le clan si ce n'est pas déjà fait !" )//
+		//général.sendMessage('Bienvenue sur le serveur ' + member.displayName + " ! N'ésite pas aller voir les #infos_du_clan et les #règles ainsi que de rejoindre le clan si ce n'est pas déjà fait !" )
 	}).catch(console.error)
 	
 	}
