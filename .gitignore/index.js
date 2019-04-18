2b2e8e1d6a788ad18657d2eaeebb64a9a0b16288
Leyks <49054110+Leyks@users.noreply.github.com>
2019-04-18 21:11:21 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,7 +2,8 @@ const Discord = require('discord.js')
 const bot = new Discord.Client()
 
 bot.on('ready', function () {
-	bot.user.setActivity('Le flood', { type: 'LISTENING' })
+	bot.user.setActivity('Deezer Music', { type: 'LISTENING' })
+	bot.user.setActivity('Le flood', { type: 'WRITING
 	//bot.user.setGame('Tape !co').catch(console.error)//
 }
 
